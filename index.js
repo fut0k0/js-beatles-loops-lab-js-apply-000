5494fc513af6458055eaa82d54f26dda3f498ade
Christopher Cabell <cccabell@icloud.com>
2019-03-06 23:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function theBeatlesPlay(musician, instrument) {
   var band = [];
-  
+  for (i = 0; i < musician.length; i++) {
+    band[i] = `${musician[i]} plays ${instrument[i]}`;
+  }
+  return band;
 }
+
